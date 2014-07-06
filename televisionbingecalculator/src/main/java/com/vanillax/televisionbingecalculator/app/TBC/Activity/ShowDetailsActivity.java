ac50cc893c4979e221b9749e716a0e07a0d040f3
Mitch Ross <mitchross09@gmail.com>
2014-07-06 19:28:31 -0400

    clean up

--- televisionbingecalculator/src/main/java/com/vanillax/televisionbingecalculator/app/TBC/Activity/ShowDetailsActivity.java
+++ televisionbingecalculator/src/main/java/com/vanillax/televisionbingecalculator/app/TBC/Activity/ShowDetailsActivity.java
@@ -3,8 +3,6 @@ package com.vanillax.televisionbingecalculator.app.TBC.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -62,28 +60,5 @@ public class ShowDetailsActivity extends Activity {
 		super.onNewIntent(intent);
 		setIntent(intent);
 	}
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.show_details, menu);
-        return true;
-    }
-
-	@Override
-	public boolean onOptionsItemSelected( MenuItem item )
-	{
-			switch ( item.getItemId() )
-			{
-				case android.R.id.home:
-					finish();
-					return true;
-			}
-
-		return super.onOptionsItemSelected( item );
-	}
-
-
 
 }
--- televisionbingecalculator/src/main/java/com/vanillax/televisionbingecalculator/app/TBC/LandingActivityMain.java
+++ televisionbingecalculator/src/main/java/com/vanillax/televisionbingecalculator/app/TBC/LandingActivityMain.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -118,24 +116,6 @@ public class LandingActivityMain extends Activity  {
 	}
 
 
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.landing_activity_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 	protected void calculateBingeTimeAndNavigate(ShowQueryMasterResponse myShow)
 	{
